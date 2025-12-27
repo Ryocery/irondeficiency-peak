f9b7f911b04a4c08e42dcdc26c134ef676cbf0a9
Ryocery <youri.haentjens@gmail.com>
2025-12-27 15:11:33 +0100

    Remove unused Instance property and streamline Config initialization in Plugin class

--- src/IronDeficiency/Plugin.cs
+++ src/IronDeficiency/Plugin.cs
@@ -8,18 +8,15 @@ namespace IronDeficiency;
 
 [BepInAutoPlugin]
 public partial class Plugin : BaseUnityPlugin {
-    public static Plugin Instance { get; private set; } = null!;
     public new static ManualLogSource Logger { get; private set; } = null!;
-    public Config ModConfig { get; private set; } = null!;
 
     private Harmony _harmony = null!;
 
     private void Awake() {
-        Instance = this;
         Logger = base.Logger;
 
         Logger.LogInfo($"Plugin {Name} is loading...");
-        ModConfig = new Config(Config);
+        new Config(Config);
 
         string? pluginDir = Path.GetDirectoryName(Info.Location);
         if (string.IsNullOrEmpty(pluginDir)) pluginDir = Paths.PluginPath;
